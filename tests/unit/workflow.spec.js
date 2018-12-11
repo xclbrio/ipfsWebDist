cc2e6d54046c597253f56de26512e0ad4dfa3cbb
Ruzerio <ruzero.io@gmail.com>
2018-12-11 14:50:44 +0300

    migrate mocha

--- tests/unit/workflow.spec.js
+++ tests/unit/workflow.spec.js
@@ -1,22 +1,21 @@
 import { expect } from 'chai'
 import { mount} from '@vue/test-utils'
 // // import workflow from '@/components/workflow.vue'
-
-import Web3 from 'web3'
+import Web3 from'web3'
 
 const web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io"));
 
 
 
-// // describe('workflow', () => {
-// // 	it('connect web3', () => {
-// // 		const wrapper = mount(workflow);
+// describe('workflow', () => {
+// 	it('connect web3', () => {
+// 		const wrapper = mount(workflow);
 
-// // 		wrapper.setData({
-// // 			web3: web3
-// // 		})
-// // 	})
-// // })
+// 		wrapper.setData({
+// 			web3: web3
+// 		})
+// 	})
+// })
 
 
 console.log(web3);
