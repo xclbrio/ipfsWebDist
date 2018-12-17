7abd263b2967bcc5656c1ed8e11cf6b94aed5cfc
Ruzerio <ruzero.io@gmail.com>
2018-12-17 21:41:07 +0300

    path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist/index.html"));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
