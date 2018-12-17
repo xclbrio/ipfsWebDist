cd10cdad32b17908e243acf20876f2b39f866d3a
Ruzerio <ruzero.io@gmail.com>
2018-12-17 21:57:38 +0300

    update server

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express'),
+	port = process.env.PORT || 4000;
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+// App setup
+const app = express();
+var server = app.listen(port, function(){
+    console.log('Started on port: ' + port);
+});
+
+// redirect
+// app.all('*', function(req, res, next) {
+// 	if (req.headers["x-forwarded-proto"] === "https"){
+//        return next();
+//     }
+//     res.redirect("https://" + req.headers.host + req.url);
+// })
+
+// token auth
+app.all('/', function(req, res) {
+	// if (!inviteCode.includes(req.query.token)) {
+	// 	return res.send('Close Alpha testing: Invalid invite token');
+	// } else {
+	// 	res.sendFile(__dirname + '/public/index.html')
+	// }
+	res.sendFile(__dirname + '/dist')
+})
+
+// Static files
+app.use(express.static('public'));
