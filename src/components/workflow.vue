b903d0ef8a396fffbf7291e6457bb6d5bbb7f801
Ruzerio <ruzero.io@gmail.com>
2018-12-17 19:40:29 +0300

    proc

--- src/components/workflow.vue
+++ src/components/workflow.vue
@@ -283,15 +283,6 @@
 		transition: 0.8s;
 		overflow: hidden;
 	}
-	.window{
-		padding: 14px 5px 5px 5px;
-		background-color: $black-three;
-		color: #fff;
-		border: 1px solid  $black;
-		box-sizing: border-box;
-		transition: 0.8s;
-		overflow: hidden;
-	}
 	.aside-left,
 	.aside-right{
 		height: 100%;
